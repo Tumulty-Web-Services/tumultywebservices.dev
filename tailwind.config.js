4c07dbc34fe4a5a93de6bae499914f17a34799c0
Peter Tumulty <ptumulty923@gmail.com>
2021-04-26 19:31:40 -0400

    added a new banner to background

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
     theme: {
-      extend: {}
+      extend: {
+        backgroundImage: theme => ({
+          'hero-pattern': "url('/assets/images/banner-2000x680.jpg')",
+         })
+      }
     },
     purge: [
       './src/**/*.njk',
