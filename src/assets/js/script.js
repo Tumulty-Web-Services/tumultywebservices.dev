cc7f6c346178d5b61aa84b9e650725945fce336a
Peter Tumulty <ptumulty923@gmail.com>
2021-04-28 20:27:30 -0400

    still tweaking the project carousel

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,27 +1,22 @@
 window.addEventListener('DOMContentLoaded', () => {
   $('.projects').slick({
+    infinite: true,
     slidesToShow: 3,
     slidesToScroll: 3,
     autoplay: true,
     autoplaySpeed: 2252,
     mobileFirst: true,
-    centerMode: true,
+    arrows: false,
     responsive: [
       {
         breakpoint: 768,
         settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
           slidesToShow: 3
         }
       },
       {
         breakpoint: 480,
         settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
           slidesToShow: 1
         }
       }
